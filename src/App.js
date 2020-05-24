61ee401138f8dce99f9f34e731b1aeb1bec149a4
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-05-23 22:17:09 -0400

    added name and occupation + made it responsive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          github push here
-        </a>
-      </header>
+      <div className="main">
+        <h1 className="name">OTABEK</h1>
+        <p >Full-Stack Web Developer</p>
+      </div>
     </div>
   );
 }
