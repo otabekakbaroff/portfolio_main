e36673ce51f6119840baf4ecc31db6c925fc354b
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-05-24 12:24:43 -0400

    added navigation bar+ responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Navbar from "./components/navbar"
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <div className="main">
         <h1 className="name">OTABEK</h1>
         <p >Full-Stack Web Developer</p>
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Navbar(){
+    return (
+        <div className="nav">
+            <a href="#Home">Home</a>
+            <a href="#Projects">Projects</a>
+            <a href="#About">About</a>
+        </div>
+    )
+}
+
+
+
+export default Navbar
