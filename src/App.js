cbdd7ae3c66c2624aba5102eea54b56e9ac99954
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-05-24 14:40:31 -0400

    hamburger navbar for mobile added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/navbar"
 function App() {
+  function sayHello(){
+    let navbar=document.querySelector(".nav");
+    if(navbar.style.display=="flex"){
+          navbar.style.display="none";
+    }else{
+        navbar.style.display="flex";
+      }
+    }
   return (
     <div className="App">
       <Navbar/>
+      <div id="sandwich" onClick={sayHello}>
+        <div id="layerOne"></div>
+        <div id="layerTwo"></div>
+        <div id="layerThree"></div>
+      </div>
       <div className="main">
         <h1 className="name">OTABEK</h1>
         <p >Full-Stack Web Developer</p>
