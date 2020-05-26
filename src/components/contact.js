1a8957428b0d4f8517b6de214bce7790784fa16f
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-05-25 20:35:14 -0400

    added contact form, needs more styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function Contact(){
     return(
-        <div>
-            <h1>Contact</h1>
+        <div className="contact">
+            <h1>Let's Connect</h1>
+            <form>
+            <h4>Feel free to leave a feedback/question</h4>
+                <input placeholder="Name"/>
+                <input placeholder="Email"/>
+                <input placeholder="Subject"/>
+                <textarea placeholder="Message"></textarea>
+            </form>
         </div>
     )
 }
