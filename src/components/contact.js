d80588b20c7d37de1fbaf2c13b319444580baf9f
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-05-26 10:25:38 -0400

    fixed form

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,13 +3,16 @@ import React from 'react';
 function Contact(){
     return(
         <div className="contact">
-            <h1>Let's Connect</h1>
+            <h1>Get in touch</h1>
             <form>
-            <h4>Feel free to leave a feedback/question</h4>
+            <h4>Feel free to leave a feedback/questions</h4>
                 <input placeholder="Name"/>
                 <input placeholder="Email"/>
                 <input placeholder="Subject"/>
                 <textarea placeholder="Message"></textarea>
+                <div className="CT_button">
+                    <button >Send</button>
+                </div>
             </form>
         </div>
     )
