5aeafe48d10d9f13f7b362249de3c781eba0fbeb
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-06-10 15:16:49 -0400

    added about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function About(){
     return(
         <div className="about">
             <h1>About Me</h1>
+            <p>
+                Hello, my name is Otabek Akbarov, I am Full Stack Web Developer from New York with passion and curriosity towards programming and technology.
+            </p>
+            <div className="aboutLinks">
+                <Link to="contact">Contact Me</Link>
+                <Link to="projects">My work</Link>
+            </div>
         </div>
     )
 }
