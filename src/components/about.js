784776fa340cd9c43e4f435f4d4a12e1b27119cd
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-06-11 12:27:39 -0400

    fixed about page for mobile

--- src/components/about.js
+++ src/components/about.js
@@ -5,9 +5,11 @@ function About(){
     return(
         <div className="about">
             <h1>About Me</h1>
-            <p>
-                Hello, my name is Otabek Akbarov, I am Full Stack Web Developer from New York with passion and curriosity towards programming and technology.
-            </p>
+            <div className="p">
+                <p>Hello, my name is Otabek Akbarov,
+                 I am Full Stack Web Developer from
+                New York with passion and curiosity towards programming and technology.</p>
+            </div>
             <div className="aboutLinks">
                 <Link to="contact">Contact Me</Link>
                 <Link to="projects">My work</Link>
