e977d8df9960b82ac994c50d612be24094420c12
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-08-05 11:20:54 -0400

    testing netlify

--- src/components/about.js
+++ src/components/about.js
@@ -9,6 +9,7 @@ function About(){
                 <p>Hello, my name is Otabek Akbarov,
                  I am Full Stack Web Developer from
                 New York with passion and curiosity towards programming and technology.</p>
+                <p>Test Back at it again</p>
             </div>
             <div className="aboutLinks">
                 <Link to="contact">Contact Me</Link>
