8b91475d584b5c0dac75d17af69a5dae72fcad86
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-08-10 14:04:28 -0400

    about page responsiveness only landscape left

--- src/components/about.js
+++ src/components/about.js
@@ -9,14 +9,15 @@ function About(){
                 <p>Hello, my name is Otabek Akbarov,
                  I am Full Stack Web Developer from
                 New York with passion and curiosity towards programming and technology.
-                Most of my time i spend coding, learning new technology or coming up with new project ideas. 
-                Off coding i spend time with my friends, going to gym, learning something new about stock market, real estate and finance in general.  
                 </p>
-
-            </div>
-            <div className="aboutLinks">
-                <Link to="contact">Contact Me</Link>
-                <Link to="projects">My work</Link>
+                <br></br>
+                <p>
+                    Don't hesitate to look around my portfolio <Link to="projects">projects.</Link> 
+                </p>
+                <br></br>
+                <p>
+                    Feel free to <Link to="contact">contact me</Link> regarding any questions you have.
+                </p>
             </div>
         </div>
     )
