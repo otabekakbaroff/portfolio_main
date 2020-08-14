17b06fe797860ea8ac1d19ccae6e36b1b6eb7dfe
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-08-14 14:36:57 -0400

    filling up protfolio with projects

--- src/components/skillz.js
+++ src/components/skillz.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import csslesspic from '../cssless.PNG'
+
 function skillz(){
     return(
-        <div>
+        <div class="skillz">
             <h1>Projects</h1>
-            
+            <div className="cssless">
+                <img src={csslesspic}/>
+            </div>
         </div>
     )
 }
--- src/components/social-link.js
+++ src/components/social-link.js
@@ -3,16 +3,17 @@ import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faLinkedin} from "@fortawesome/free-brands-svg-icons"
 import {faGithub} from "@fortawesome/free-brands-svg-icons"
 import {faGoogle} from "@fortawesome/free-brands-svg-icons"
-import {faTwitter} from "@fortawesome/free-brands-svg-icons"
+
 
 
 function Social_link(){
+    let replitLogo="https://icomoon.io/icons39f00d9/4/666.svg"
     return(
         <div id ="share" className="hover">
           <a className="social-link" href="https://github.com/otabekakbaroff" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub}/></a>
           <a className="social-link" href="https://www.linkedin.com/in/otabek-akbarov-163a22198/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
           <a className="social-link" href="mailto:otabek.akbaroff@gmail.com" rel="noopener noreferrer"><FontAwesomeIcon icon={faGoogle}/></a>
-          <a className="social-link" href="https://twitter.com/Otabek83809016" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+          <a className="social-link" href="https://repl.it/@otabek99" target="_blank" rel="noopener noreferrer"><img src={replitLogo}/></a>
        </div> 
     )
 }
