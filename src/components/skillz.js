9b0d87cd845f82a00eb8b36ac4b24bf69214a28a
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-08-14 20:50:57 -0400

    contact form new design

--- src/components/skillz.js
+++ src/components/skillz.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import csslesspic from '../cssless.PNG'
 
+
 function skillz(){
     return(
         <div class="skillz">
             <h1>Projects</h1>
-            <div className="cssless">
-                <img alt= "css less project" src={csslesspic}/>
-            </div>
+            <div className="projects"></div>
+            {/* <br></br> */}
+                {/* <div className="cssless">
+                    <a href="https://css-less.netlify.app/" target="_blank"><img alt= "css less project" src={csslesspic}/></a>
+                </div> */}
         </div>
     )
 }
