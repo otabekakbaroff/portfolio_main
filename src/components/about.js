80b6161146804a3cbef3d41bdb253bc725c8baf8
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-08-16 18:11:11 -0400

    about page updates

--- src/components/about.js
+++ src/components/about.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faReact,faGit,faLess,faNodeJs,faNode,faPython} from "@fortawesome/free-brands-svg-icons"
 
 function About(){
     return(
-        <div className="about">
+        <div className="mainAbout">
             <h1>About Me</h1>
+        <div className="about">
             <div className="p">
                 <p>Hello, my name is Otabek Akbarov,
                  I am Full Stack Web Developer from
-                New York with passion and curiosity towards programming and technology.
+                New York with passion and curiosity towards technology and hunders of hours of coding experience.
                 </p>
                 <br></br>
                 <p>
@@ -19,6 +22,15 @@ function About(){
                     Feel free to <Link to="contact">contact me</Link> regarding any questions you have.
                 </p>
             </div>
+            <div className="aboutSkillzSection">
+                <div className="aboutLogos" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGit}/></div>
+                <div className="aboutLogos" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faReact}/></div>
+                <div className="aboutLogos" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLess}/></div>
+                <div className="aboutLogos" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faNodeJs}/></div>
+                <div className="aboutLogos" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faNode}/></div>
+                <div className="aboutLogos" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faPython}/></div>
+            </div>
+        </div>
         </div>
     )
 }
--- src/components/skillz.js
+++ src/components/skillz.js
@@ -7,10 +7,6 @@ function skillz(){
         <div class="skillz">
             <h1>Projects</h1>
             <div className="projects"></div>
-            {/* <br></br> */}
-                {/* <div className="cssless">
-                    <a href="https://css-less.netlify.app/" target="_blank"><img alt= "css less project" src={csslesspic}/></a>
-                </div> */}
         </div>
     )
 }
