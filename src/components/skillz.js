439255c7925a4cfc689d0c5365db8e8224e40d3e
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-08-21 11:54:16 -0400

    projects style

--- src/components/skillz.js
+++ src/components/skillz.js
@@ -6,7 +6,48 @@ function skillz(){
     return(
         <div class="skillz">
             <h1>Projects</h1>
-            <div className="projects"></div>
+            <div className="projects">
+                <div className="css-less">
+                    <div className="css-less-img"></div>
+                    <p>
+                        This project is about something that has to do with something, <span className="skill-stack">python</span> might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up
+                    </p>
+                    <div className="visit_buttons">
+                        <button>Source Code</button>
+                        <button>Live Site</button>
+                    </div>
+                </div>
+                <div className="css-less">
+                    <div className="css-less-img"></div>
+                    <p>
+                        This project is about something that has to do with something, might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up 
+                    </p>
+                    <div className="visit_buttons">
+                        <button>Source Code</button>
+                        <button>Live Site</button>
+                    </div>
+                </div>
+                <div className="css-less">
+                    <div className="css-less-img"></div>
+                    <p>
+                        This project is about something that has to do with something, might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up
+                    </p>
+                    <div className="visit_buttons">
+                        <button>Source Code</button>
+                        <button>Live Site</button>
+                    </div>
+                </div>
+                <div className="css-less">
+                    <div className="css-less-img"></div>
+                    <p>
+                    This project is about something that has to do with something, might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up
+                    </p>
+                    <div className="visit_buttons">
+                        <button>Source Code</button>
+                        <button>Live Site</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/social-link.js
+++ src/components/social-link.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faLinkedin} from "@fortawesome/free-brands-svg-icons"
 import {faGithub} from "@fortawesome/free-brands-svg-icons"
-import {faGoogle} from "@fortawesome/free-brands-svg-icons"
+
 
 
 
@@ -12,7 +12,7 @@ function Social_link(){
         <div id ="share" className="hover">
           <a className="social-link" href="https://github.com/otabekakbaroff" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub}/></a>
           <a className="social-link" href="https://www.linkedin.com/in/otabek-akbarov-163a22198/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
-          <a className="social-link" href="mailto:otabek.akbaroff@gmail.com" rel="noopener noreferrer"><FontAwesomeIcon icon={faGoogle}/></a>
+          <a className="social-link" href="https://resume.io/r/fuifzJ2DP" rel="noopener noreferrer" target="_blank"><h6>Resume</h6></a>
           <a className="social-link" href="https://repl.it/@otabek99" target="_blank" rel="noopener noreferrer"><img alt="replit" src={replitLogo}/></a>
        </div> 
     )
