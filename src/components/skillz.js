1241beafd456b68c01e4d7fbc363f43c6227a801
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-08-30 15:13:25 -0400

    fixed deploy errors

--- src/components/skillz.js
+++ src/components/skillz.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 function skillz(){
     return(
-        <div class="skillz">
+        <div className="skillz">
             <h1>Projects</h1>
             <div className="projects">
                 <div className="css-less">
@@ -13,20 +13,18 @@ function skillz(){
                     <span className="highlight">Tables Thursday</span> project purpose to help people organize, and manage their tasks in a easier way. Project was built in a <span className="highlight">team of 7</span>. I was responsible to <span className="highlight">build Home page using HTML CSS-Less and Javascript</span>. 
                     </p>
                     <div className="visit_buttons">
-                        <a href="https://github.com/Build-Week-TablessThursday/UI" target="_blank" rel="noopener noreferrer">Source Code</a>
+                        <a href="https://github.com/Build-Week-TablessThursday/UI" target="_blank" rel="noopener noreferrer" className="source">Source Code</a>
                         <a href="https://tables-thursday.netlify.app/" target="_blank" rel="noopener noreferrer">Live Site</a>
                     </div>
                 </div>
                 <div className="css-less">
                     <div className="css-less-img" id="css-less"></div>
-                    <br></br>
                     <p>
                         <span className="highlight">SpaceWalkers</span> was Lambda School Challenge Project practicing <span className="highlight">css-less</span>
                     </p>
-                    <br></br>
                     <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects">Source Code</a>
-                        <a href="https://otabekakbaroff.com/projects">Live Site</a>
+                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
+                        <a href="https://otabekakbaroff.com/projects" className="live">Live Site</a>
                     </div>
 
                 </div>
@@ -36,7 +34,7 @@ function skillz(){
                         This project is about something that has to do with something, might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up
                     </p>
                     <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects">Source Code</a>
+                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
                         <a href="https://otabekakbaroff.com/projects">Live Site</a>
                     </div>
                 </div>
@@ -46,7 +44,7 @@ function skillz(){
                     This project is about something that has to do with something, might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up
                     </p>
                     <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects">Source Code</a>
+                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
                         <a href="https://otabekakbaroff.com/projects">Live Site</a>
                     </div>
                 </div>
@@ -56,7 +54,7 @@ function skillz(){
                     This project is about something that has to do with something, might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up
                     </p>
                     <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects">Source Code</a>
+                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
                         <a href="https://otabekakbaroff.com/projects">Live Site</a>
                     </div>
                 </div>
@@ -66,7 +64,7 @@ function skillz(){
                     This project is about something that has to do with something, might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up
                     </p>
                     <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects">Source Code</a>
+                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
                         <a href="https://otabekakbaroff.com/projects">Live Site</a>
                     </div>
                 </div>
@@ -76,7 +74,7 @@ function skillz(){
                     This project is about something that has to do with something, might be fitness app, might be  finance app or even traveling app etc big sentences and etc more just typing to fill this place up
                     </p>
                     <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects">Source Code</a>
+                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
                         <a href="https://otabekakbaroff.com/projects">Live Site</a>
                     </div>
                 </div>
