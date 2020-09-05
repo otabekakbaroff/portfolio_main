0086cdc8290b17f570219733f568fa9ce1619f38
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-09-05 15:04:09 -0400

    removed unneccery items

--- src/components/skillz.js
+++ src/components/skillz.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import csslesspic from '../cssless.PNG'
+
 
 
 function skillz(){
@@ -10,7 +10,7 @@ function skillz(){
                 <div className="css-less">
                     <div className="css-less-img" id="tables-thursday"></div>
                     <p>
-                    <span className="highlight">Tables Thursday</span> project purpose to help people organize, and manage their tasks in a easier way. Project was built in a <span className="highlight">team of 7</span> with <span className="highlight">4 day deadline</span>. I was responsible to <span className="highlight">build Home page using HTML CSS-Less and Javascript</span>. 
+                    <span className="highlight">Tables Thursday</span> Project purpose to help people organize, and manage their tasks in a easier way. Project was built in a <span className="highlight">team of 7</span> with <span className="highlight">4 days deadline</span>. I was responsible to <span className="highlight">build Home page using HTML CSS-Less and Javascript</span>. 
                     </p>
                     <br></br>
                     <div className="visit_buttons">
@@ -19,21 +19,9 @@ function skillz(){
                     </div>
                 </div>
                 <div className="css-less">
-                    <div className="css-less-img" id="css-less"></div>
-                    <p>
-                        <span className="highlight">SpaceWalkers</span> was Lambda School Challenge Project practicing <span className="highlight">CSS-Less</span>
-                    </p>
-                    <br></br>
-                    <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
-                        <a href="https://otabekakbaroff.com/projects" className="live">Live Site</a>
-                    </div>
-
-                </div>
-                <div className="css-less">
                     <div className="css-less-img" id="business-card"></div>
                     <p>
-                        <span className="highlight">Business-card-organizer</span> project purple to help people organize their business cards in a simplest way posibile. Project was built in a <span className="highlight">team of 8 </span> with <span className="highlight">4 day deadline.</span> I was responsible to build <span className='highlight'>Front End</span> with two of my other teammates using <span>React</span>. My part was to make <span className="highlight">API calls</span> to backend, receive, manipulate, store and display the data.
+                        <span className="highlight">Business-card-organizer</span> project purpose to help people organize their business cards in a simplest way posibile. Project was built in a <span className="highlight">team of 8 </span> with <span className="highlight">4 days deadline.</span> I was responsible to build <span className='highlight'>Front End</span> with two of my other teammates using <span>React</span>. My part was to make <span className="highlight">API calls</span> to backend, receive, manipulate, store and display the data.
                     </p>
                     <br></br>
                     <div className="visit_buttons">
@@ -44,7 +32,7 @@ function skillz(){
                 <div className="css-less">
                     <div className="css-less-img" id="anywhere-fitness"></div>
                     <p>
-                        <span className="highlight">Anywhere-fitness</span> initially was Lambda School team project, i was responsible to Build <span className="highlight">Back End</span> for my team using <span className="highlight">Node, Express, and SQLite</span>. I enjoyed the project so decided to build the Front End for it too. The project makes it easy for instructors to create classes, keep track of attandance and payments. Students can have flexability of chosing where, when, and with who they want to workout with.  
+                        <span className="highlight">Anywhere-fitness</span> initially was Lambda School team project, i was responsible to Build <span className="highlight">Back End</span> for my team using <span className="highlight">Node, Express, and SQLite</span>. I enjoyed the project so decided to build the Front End for it too. The project makes it easy for instructors to create classes, keep track of attandance and payments. Students can have flexability of chosing where, when, and who's their instructor.  
                     </p>
                     <br></br>
                     <div className="visit_buttons">
