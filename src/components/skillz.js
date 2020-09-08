4b493bc6457abb8716f1b5e5cf64551e3f4d3a3f
Otabek Akbarov <Otabek.Akbaroff@gmail.com>
2020-09-08 18:36:34 -0400

    added projects

--- src/components/skillz.js
+++ src/components/skillz.js
@@ -8,25 +8,26 @@ function skillz(){
             <h1>Projects</h1>
             <div className="projects">
                 <div className="css-less">
-                    <div className="css-less-img" id="tables-thursday"></div>
-                    <p>
-                    <span className="highlight">Tables Thursday</span> Project purpose to help people organize, and manage their tasks in a easier way. Project was built in a <span className="highlight">team of 7</span> with <span className="highlight">4 days deadline</span>. I was responsible to <span className="highlight">build Home page using HTML CSS-Less and Javascript</span>. 
-                    </p>
-                    <br></br>
-                    <div className="visit_buttons">
-                        <a href="https://github.com/Build-Week-TablessThursday/UI" target="_blank" rel="noopener noreferrer" className="source">Source Code</a>
-                        <a href="https://tables-thursday.netlify.app/" target="_blank" rel="noopener noreferrer" className="live">Live Site</a>
-                    </div>
+                        <div className="css-less-img" id="business-card"></div>
+                        <p>
+                            <span className="highlight">Business-card-organizer</span> project purpose to make it easy for people to organize  and collect their business cards. Project was built in a <span className="highlight">team of 8 </span> with <span className="highlight">4 days deadline.</span> I was responsible to build <span className='highlight'>Front End</span> with two of my other teammates using <span>React</span>. My part was to make <span className="highlight">API calls</span> to backend, receive, manipulate, store and display the data.
+                        </p>
+                        <br></br>
+                        <div className="visit_buttons">
+                            <a href="https://github.com/Build-Week-Business-Card-Organizer-2/Front-End" target="new" className="source">Source Code</a>
+                            <a href="https://businesscard-organizer.netlify.app/" target="new" className="live">Live Site</a>
+                        </div>
                 </div>
                 <div className="css-less">
-                    <div className="css-less-img" id="business-card"></div>
+                    <div className="css-less-img" id="chattychat"></div>
                     <p>
-                        <span className="highlight">Business-card-organizer</span> project purpose to help people organize their business cards in a simplest way posibile. Project was built in a <span className="highlight">team of 8 </span> with <span className="highlight">4 days deadline.</span> I was responsible to build <span className='highlight'>Front End</span> with two of my other teammates using <span>React</span>. My part was to make <span className="highlight">API calls</span> to backend, receive, manipulate, store and display the data.
+                        <span className="highlight">Chattychat</span> is an app where you can join random chat rooms where you can chat with random people or create your own chat room and invite your friends.
                     </p>
                     <br></br>
-                    <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
-                        <a href="https://otabekakbaroff.com/projects" className="live">Live Site</a>
+                    <div className="visit_buttons" id="backend">
+                        <a href=" https://github.com/otabekakbaroff/chatapp"  target="new" className="source">FE Source Code</a>
+                        <a href="https://github.com/otabekakbaroff/chatappbe"  target="new" className="source">BE Source Code</a>
+                        <a href="https://chattychat.netlify.app/"  target="new" className="live">Live Site</a>
                     </div>
                 </div>
                 <div className="css-less">
@@ -35,9 +36,10 @@ function skillz(){
                         <span className="highlight">Anywhere-fitness</span> initially was Lambda School team project, i was responsible to Build <span className="highlight">Back End</span> for my team using <span className="highlight">Node, Express, and SQLite</span>. I enjoyed the project so decided to build the Front End for it too. The project makes it easy for instructors to create classes, keep track of attandance and payments. Students can have flexability of chosing where, when, and who's their instructor.  
                     </p>
                     <br></br>
-                    <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
-                        <a href="https://otabekakbaroff.com/projects" className="live">Live Site</a>
+                    <div className="visit_buttons" id="backend">
+                        <a href="https://github.com/otabekakbaroff/anywhere_fitnessFE"  target="new" className="source">FE Source Code</a>
+                        <a href="https://github.com/otabekakbaroff/anywhere_fitness"  target="new" className="source">BE Source Code</a>
+                        <a href="https://anywherefitnessfe.netlify.app/"  target="new" className="live">Live Site</a>
                     </div>
                 </div>
                 <div className="css-less">
@@ -47,19 +49,18 @@ function skillz(){
                     </p>
                     <br></br>
                     <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
-                        <a href="https://otabekakbaroff.com/projects" className="live">Live Site</a>
+                        <a href="https://mealfresh.us/" target="new" className="live">Live Site</a>
                     </div>
                 </div>
                 <div className="css-less">
-                    <div className="css-less-img" id="chattychat"></div>
+                    <div className="css-less-img" id="tables-thursday"></div>
                     <p>
-                        <span className="highlight">Chattychat</span> is an app where you can join random chat rooms where you can chat with random people or create your own chat room and invite your friends.
+                    <span className="highlight">Tables Thursday</span> Project purpose to help people organize, and manage their tasks in a easier way. Project was built in a <span className="highlight">team of 7</span> with <span className="highlight">4 days deadline</span>. I was responsible to <span className="highlight">build Home page using HTML CSS-Less and Javascript</span>. 
                     </p>
                     <br></br>
                     <div className="visit_buttons">
-                        <a href="https://otabekakbaroff.com/projects" className="source">Source Code</a>
-                        <a href="https://otabekakbaroff.com/projects" className="live">Live Site</a>
+                        <a href="https://github.com/Build-Week-TablessThursday/UI" target="new" className="source">Source Code</a>
+                        <a href="https://tables-thursday.netlify.app/" target="new"  className="live">Live Site</a>
                     </div>
                 </div>
             </div>
